420213c050bcf40a44f1657bff57f8ec06d7e881
Veerendra Raghuwanshi <68636328+veer-raghuwanshi@users.noreply.github.com>
2023-04-04 14:23:48 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+// alert("hello world");
+// document.getElementById("h").innerHTML = "Veer";
+// document.write("Veer");
+// console.log("hello world");
+
+// var a;
+// a=9;
+// // alert(a);
+// var digit1 = 5;
+// alert(digit1);
+// var x = 5;
+// var z = digit1+x;
+// console.log(z);
+
+// let digit1 = 1;
+// let x = 5;
+// let z = digit1+x;
+// console.log(z);
+
+// let digit1 = 1;
+// const x = 5;
+// digit1 = 5;
+// // let z = digit1+x;
+
+// console.log(digit1);
+
+
+// let str = " veer";
+
+// let greet = `Hi ${str}`;
+// console.log(greet);
+
+let str = " this is javascript .it is a good tutorial. ";
+let trimedstr = str.trim();
+console.log(str);
+console.log(trimedstr);
